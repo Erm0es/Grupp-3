b62d5c7e298c51722e9d368dcf6e10c24e9cd247
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-02-19 11:41:26 +0100

    added localhost test

--- cypress/integration/sample_test.js
+++ cypress/integration/sample_test.js
@@ -1,15 +1,17 @@
-context('Assertions', () => {
-  beforeEach(() => {
+describe('testing localhost', () => {
+  it('tests localhost', () => {
+    cy.visit('http://127.0.0.1:5502/index.html');
+  });
+});
+
+describe('Implicit Assertions', () => {
+  it('tests if .all-challenges have text "Go to all challenges"', () => {
     cy.visit('https://erm0es.github.io/Grupp-3/');
+    cy.get('.all-challenges').should('have.text', 'Go to all challenges');
   });
 
-  describe('Implicit Assertions', () => {
-    it('tests if .all-challenges have text "Go to all challenges"', () => {
-      cy.get('.all-challenges').should('have.text', 'Go to all challenges');
-    });
-
-    it('clicks on CTA "Go to all challenges"', () => {
-      cy.get('.all-challenges').click();
-    });
+  it('clicks on CTA "Go to all challenges"', () => {
+    cy.visit('https://erm0es.github.io/Grupp-3/');
+    cy.get('.all-challenges').click();
   });
 });
